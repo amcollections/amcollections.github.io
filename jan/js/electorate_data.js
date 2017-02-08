935480fa1b12bbdac27d24c6d3fe154865b65e72
adamrmor <adamrmor@users.noreply.github.com>
2017-02-08 13:27:41 +1300

    Update electorate_data.js

--- jan/js/electorate_data.js
+++ jan/js/electorate_data.js
@@ -2,228 +2,228 @@ var electorates = {
   "results": {
   "1": {
     "electoral_district": "Torbay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "2": {
     "electoral_district": "Oteha",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "3": {
     "electoral_district": "Browns Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "4": {
     "electoral_district": "Rothesay Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "5": {
     "electoral_district": "Pinehill",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "6": {
     "electoral_district": "Albany",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "7": {
     "electoral_district": "Windsor Park",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "8": {
     "electoral_district": "Murrays Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "9": {
     "electoral_district": "Unsworth Heights",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "10": {
     "electoral_district": "Sunnynook",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "11": {
     "electoral_district": "Mairangi Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "12": {
     "electoral_district": "Totara Vale",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "13": {
     "electoral_district": "Forrest Hill",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "14": {
     "electoral_district": "Campbells Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "15": {
     "electoral_district": "Greenhithe",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "16": {
     "electoral_district": "Bay View",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "17": {
     "electoral_district": "Glenfield",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "18": {
     "electoral_district": "Castor Bay",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "19": {
     "electoral_district": "Birkdale",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "20": {
     "electoral_district": "Hillcrest",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "21": {
     "electoral_district": "Nothcote",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "22": {
     "electoral_district": "Milford",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "23": {
     "electoral_district": "Takapuna",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "24": {
     "electoral_district": "Hauraki",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "25": {
     "electoral_district": "Belmont",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "26": {
     "electoral_district": "Baywater",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "27": {
     "electoral_district": "Narrowneck",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "28": {
     "electoral_district": "Devonport",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "29": {
     "electoral_district": "Beachhaven",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "30": {
     "electoral_district": "Birkenhead",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "31": {
     "electoral_district": "Northcote Point",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   },
   "32": {
     "electoral_district": "Chatwood",
-    "valid_winning_candidate_votes": 100,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
-    "total_votes_cast": 200,
-    "": "National Party"
+    "valid_winning_candidate_votes": 13465,
+    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
+    "total_votes_cast": 31857,
+    "party": "National Party"
   }
-  }, 
-  "max_turnout": "6400"
+}, 
+  "max_turnout": "1019424"
 }
