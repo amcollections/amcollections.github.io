e0dccb8f94c3bea7c74f8a2552adb0d17f743677
adamrmor <adamrmor@users.noreply.github.com>
2017-02-08 13:14:00 +1300

    Update electorate_data.js

--- jan/js/electorate_data.js
+++ jan/js/electorate_data.js
@@ -1,17 +1,17 @@
 var electorates = {
   "results": {
     "07": {
-      "electorate_candidate": "BENNETT, Paula Lee", 
-      "urban": "yes", 
+     // "electorate_candidate": "BENNETT, Paula Lee", 
+     // "urban": "yes", 
       "map_id": 7, 
-      "electoral_district": "Waitakere", 
-      "valid_winning_candidate_votes": 13465, 
-      "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%", 
+      "electoral_district": "Waitakere test", 
+      "valid_winning_candidate_votes": 31857, 
+      "percentage_of_elected_candidate_votes_cast_in_electorate": "100%", 
       "total_votes_cast": 31857, 
-      "on_the_party_list": "yes", 
-      "majority": 9, 
-      "party": "National Party", 
-      "maori_electorate": "no"
+     // "on_the_party_list": "yes", 
+     // "majority": 9, 
+      "party": "labour Party", 
+     // "maori_electorate": "no"
     }, 
     "30": {
       "electorate_candidate": "ARDERN, Philip Shane", 
