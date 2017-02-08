bbd1b76d16c5c490aa9c4d67088bde107861dc26
adamrmor <adamrmor@users.noreply.github.com>
2017-02-08 14:14:33 +1300

    Update electorate_data.js

--- jan/js/electorate_data.js
+++ jan/js/electorate_data.js
@@ -1,514 +1,77 @@
 var electorates = {
   "results": {
-    "07": {
-      "electorate_candidate": "BENNETT, Paula Lee", 
-      "urban": "yes", 
-      "map_id": 7, 
-      "electoral_district": "Waitakere", 
-      "valid_winning_candidate_votes": 13465, 
-      "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%", 
-      "total_votes_cast": 31857, 
-      "on_the_party_list": "yes", 
-      "majority": 9, 
-      "party": "National Party", 
-      "maori_electorate": "no"
-    },
-    "02": {
-      "electoral_district": "Oteha",
-      "valid_winning_candidate_votes": 20049,
-      "percentage_of_elected_candidate_votes_cast_in_electorate": "59.18%",
-      "total_votes_cast": 34995,
-      "on_the_party_list": "yes",
-      "party": "National Party"
-  },
-  "03": {
-      "electoral_district": "Browns Bay",
-      "valid_winning_candidate_votes": 20253,
-      "percentage_of_elected_candidate_votes_cast_in_electorate": "53.54%",
-      "total_votes_cast": 38997,
-      "on_the_party_list": "yes",
-      "party": "National Party"
-  },
-  "04": {
-    "electoral_district": "Rothesay Bay",
-    "valid_winning_candidate_votes": 26011,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "74.38%",
-    "total_votes_cast": 36266,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "05": {
-    "electoral_district": "Pinehill",
-    "valid_winning_candidate_votes": 21094,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "64.98%",
-    "total_votes_cast": 33888,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "06": {
-    "electoral_district": "Albany",
-    "valid_winning_candidate_votes": 22709,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "62.44%",
-    "total_votes_cast": 37543,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "07": {
-    "electoral_district": "Windsor Park",
-    "valid_winning_candidate_votes": 13465,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.74%",
-    "total_votes_cast": 31857,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "08": {
-    "electoral_district": "Murrays Bay",
-    "valid_winning_candidate_votes": 18908,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "58.46%",
-    "total_votes_cast": 33454,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "09": {
-    "electoral_district": "Unsworth Heights",
-    "valid_winning_candidate_votes": 15860,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "53.83%",
-    "total_votes_cast": 31668,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
+  "1": {
+	    "electoral_district": "Torbay",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+ 		 },
+  "2": {
+	    "electoral_district": "Oteha",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+ 		 },
+  "3": {
+	    "electoral_district": "Browns Bay",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+ 		},
+  "4": {
+	    "electoral_district": "Rothesay Bay",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+  		},
+  "5": {
+	    "electoral_district": "Pinehill",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+  		},
+  "6": {
+	    "electoral_district": "Albany",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+	  	},
+  "7": {
+	    "electoral_district": "Windsor Park",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+  		},
+  "8": {
+	    "electoral_district": "Murrays Bay",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+ 	 },
+  "9": {
+	    "electoral_district": "Unsworth Heights",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+  		},
   "10": {
-    "electoral_district": "Sunnynook",
-    "valid_winning_candidate_votes": 15038,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "45.39%",
-    "total_votes_cast": 34761,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "11": {
-    "electoral_district": "Mairangi Bay",
-    "valid_winning_candidate_votes": 24837,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "67.67%",
-    "total_votes_cast": 38366,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "12": {
-    "electoral_district": "Totara Vale",
-    "valid_winning_candidate_votes": 18716,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "59.19%",
-    "total_votes_cast": 33752,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "13": {
-    "electoral_district": "Forrest Hill",
-    "valid_winning_candidate_votes": 15835,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.10%",
-    "total_votes_cast": 37185,
-    "on_the_party_list": "yes",
-    "party": "ACT New Zealand"
-  },
-  "14": {
-    "electoral_district": "Campbells Bay",
-    "valid_winning_candidate_votes": 16189,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "49.88%",
-    "total_votes_cast": 34696,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "15": {
-    "electoral_district": "Greenhithe",
-    "valid_winning_candidate_votes": 20694,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "65.86%",
-    "total_votes_cast": 32995,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "16": {
-    "electoral_district": "New Lynn",
-    "valid_winning_candidate_votes": 16999,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "52.14%",
-    "total_votes_cast": 34345,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "17": {
-    "electoral_district": "Mt Roskill",
-    "valid_winning_candidate_votes": 17906,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "57.15%",
-    "total_votes_cast": 33249,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "18": {
-    "electoral_district": "Manukau East",
-    "valid_winning_candidate_votes": 19399,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "77.11%",
-    "total_votes_cast": 28418,
-    "on_the_party_list": "no",
-    "party": "Labour Party"
-  },
-  "19": {
-    "electoral_district": "Botany",
-    "valid_winning_candidate_votes": 17780,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "64.03%",
-    "total_votes_cast": 29567,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "20": {
-    "electoral_district": "Bay View",
-    "valid_winning_candidate_votes": 18177,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "75.90%",
-    "total_votes_cast": 26528,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "21": {
-    "electoral_district": "Glenfield",
-    "valid_winning_candidate_votes": 14961,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "63.27%",
-    "total_votes_cast": 26457,
-    "on_the_party_list": "no",
-    "party": "Labour Party"
-  },
-  "22": {
-    "electoral_district": "Castor Bay",
-    "valid_winning_candidate_votes": 18096,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "59.59%",
-    "total_votes_cast": 32302,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "23": {
-    "electoral_district": "Birkdale",
-    "valid_winning_candidate_votes": 22563,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "65.59%",
-    "total_votes_cast": 35885,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "24": {
-    "electoral_district": "Hillcrest",
-    "valid_winning_candidate_votes": 19817,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "64.90%",
-    "total_votes_cast": 31970,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "25": {
-    "electoral_district": "Nothcote",
-    "valid_winning_candidate_votes": 18571,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "55.05%",
-    "total_votes_cast": 35006,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "26": {
-    "electoral_district": "Milford",
-    "valid_winning_candidate_votes": 18505,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "57.11%",
-    "total_votes_cast": 33824,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "27": {
-    "electoral_district": "Takapuna",
-    "valid_winning_candidate_votes": 16587,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "53.26%",
-    "total_votes_cast": 32592,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "28": {
-    "electoral_district": "Hauraki",
-    "valid_winning_candidate_votes": 23710,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "67.48%",
-    "total_votes_cast": 36622,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "29": {
-    "electoral_district": "Belmont",
-    "valid_winning_candidate_votes": 21971,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "61.40%",
-    "total_votes_cast": 37150,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "30": {
-    "electoral_district": "Baywater",
-    "valid_winning_candidate_votes": 20842,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "69.65%",
-    "total_votes_cast": 31461,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "31": {
-    "electoral_district": "Narrowneck",
-    "valid_winning_candidate_votes": 20934,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "63.65%",
-    "total_votes_cast": 34204,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "32": {
-    "electoral_district": "Devonport",
-    "valid_winning_candidate_votes": 17644,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "53.31%",
-    "total_votes_cast": 34334,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "33": {
-    "electoral_district": "Beachhaven",
-    "valid_winning_candidate_votes": 16743,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "53.69%",
-    "total_votes_cast": 32259,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "34": {
-    "electoral_district": "Rotorua",
-    "valid_winning_candidate_votes": 17188,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "56.17%",
-    "total_votes_cast": 32097,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "35": {
-    "electoral_district": "Birkenhead",
-    "valid_winning_candidate_votes": 14003,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "48.27%",
-    "total_votes_cast": 30264,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "36": {
-    "electoral_district": "Rangitīkei",
-    "valid_winning_candidate_votes": 18284,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "58.87%",
-    "total_votes_cast": 32308,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "37": {
-    "electoral_district": "Napier",
-    "valid_winning_candidate_votes": 17337,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "52.11%",
-    "total_votes_cast": 34468,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "38": {
-    "electoral_district": "Northcote Point",
-    "valid_winning_candidate_votes": 16525,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "51.32%",
-    "total_votes_cast": 33534,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "39": {
-    "electoral_district": "Chatwood",
-    "valid_winning_candidate_votes": 19378,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "58.42%",
-    "total_votes_cast": 34641,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "40": {
-    "electoral_district": "Ōtaki",
-    "valid_winning_candidate_votes": 19151,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "51.57%",
-    "total_votes_cast": 38098,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "41": {
-    "electoral_district": "Wairarapa",
-    "valid_winning_candidate_votes": 17881,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "52.65%",
-    "total_votes_cast": 35473,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "42": {
-    "electoral_district": "Mana",
-    "valid_winning_candidate_votes": 16323,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "48.61%",
-    "total_votes_cast": 35291,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "43": {
-    "electoral_district": "Rimutaka",
-    "valid_winning_candidate_votes": 17171,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "51.58%",
-    "total_votes_cast": 34870,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "44": {
-    "electoral_district": "Ōhariu",
-    "valid_winning_candidate_votes": 14357,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "38.58%",
-    "total_votes_cast": 38132,
-    "on_the_party_list": "yes",
-    "party": "United Future"
-  },
-  "45": {
-    "electoral_district": "Hutt South",
-    "valid_winning_candidate_votes": 15828,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "49.38%",
-    "total_votes_cast": 33550,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "46": {
-    "electoral_district": "Wellington Central",
-    "valid_winning_candidate_votes": 18836,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "49.15%",
-    "total_votes_cast": 39816,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "47": {
-    "electoral_district": "Rongotai",
-    "valid_winning_candidate_votes": 18179,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "50.52%",
-    "total_votes_cast": 37596,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "48": {
-    "electoral_district": "Nelson",
-    "valid_winning_candidate_votes": 18360,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "53.14%",
-    "total_votes_cast": 35696,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "49": {
-    "electoral_district": "Kaikōura",
-    "valid_winning_candidate_votes": 19961,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "60.25%",
-    "total_votes_cast": 34575,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "50": {
-    "electoral_district": "Waimakariri",
-    "valid_winning_candidate_votes": 16787,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "47.28%",
-    "total_votes_cast": 36516,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "51": {
-    "electoral_district": "West Coast-Tasman",
-    "valid_winning_candidate_votes": 15753,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "47.51%",
-    "total_votes_cast": 34226,
-    "on_the_party_list": "no",
-    "party": "Labour Party"
-  },
-  "52": {
-    "electoral_district": "Selwyn",
-    "valid_winning_candidate_votes": 24963,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "69.14%",
-    "total_votes_cast": 37168,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "53": {
-    "electoral_district": "Christchurch East",
-    "valid_winning_candidate_votes": 15559,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "55.54%",
-    "total_votes_cast": 29163,
-    "on_the_party_list": "no",
-    "party": "Labour Party"
-  },
-  "54": {
-    "electoral_district": "Ilam",
-    "valid_winning_candidate_votes": 20070,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "60.40%",
-    "total_votes_cast": 34551,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "55": {
-    "electoral_district": "Christchurch Central",
-    "valid_winning_candidate_votes": 12064,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.53%",
-    "total_votes_cast": 28494,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "56": {
-    "electoral_district": "Wigram",
-    "valid_winning_candidate_votes": 14080,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "45.11%",
-    "total_votes_cast": 32834,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "57": {
-    "electoral_district": "Port Hills",
-    "valid_winning_candidate_votes": 15737,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "48.41%",
-    "total_votes_cast": 33678,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "58": {
-    "electoral_district": "Rangitata",
-    "valid_winning_candidate_votes": 19580,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "55.77%",
-    "total_votes_cast": 36404,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "59": {
-    "electoral_district": "Waitaki",
-    "valid_winning_candidate_votes": 23219,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "61.45%",
-    "total_votes_cast": 39000,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "60": {
-    "electoral_district": "Dunedin South",
-    "valid_winning_candidate_votes": 16844,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "48.89%",
-    "total_votes_cast": 35692,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "61": {
-    "electoral_district": "Dunedin North",
-    "valid_winning_candidate_votes": 12976,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "44.25%",
-    "total_votes_cast": 30292,
-    "on_the_party_list": "yes",
-    "party": "Labour Party"
-  },
-  "62": {
-    "electoral_district": "Clutha-Southland",
-    "valid_winning_candidate_votes": 21375,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "68.83%",
-    "total_votes_cast": 32176,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  },
-  "63": {
-    "electoral_district": "Invercargill",
-    "valid_winning_candidate_votes": 17275,
-    "percentage_of_elected_candidate_votes_cast_in_electorate": "54.58%",
-    "total_votes_cast": 32811,
-    "on_the_party_list": "yes",
-    "party": "National Party"
-  }
-}, 
-  "max_turnout": "1019424"
+	    "electoral_district": "Sunnynook",
+	    "valid_winning_candidate_votes": 100,
+	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
+	    "total_votes_cast": 200,
+	    "party": "National Party"
+	  },
+   "max_turnout": "6400"
+}
+  }, 
+  "max_turnout": "39816"
 }
