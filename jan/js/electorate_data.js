42eb5472aeb6fd2483bde88a4a1dc8397387c4df
adamrmor <adamrmor@users.noreply.github.com>
2017-02-08 14:20:19 +1300

    Update electorate_data.js

--- jan/js/electorate_data.js
+++ jan/js/electorate_data.js
@@ -1,76 +1,75 @@
 var electorates = {
   "results": {
-  "1": {
+  	"1": {
 	    "electoral_district": "Torbay",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
- 		 },
+ 	},
   "2": {
 	    "electoral_district": "Oteha",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
- 		 },
+ 	 },
   "3": {
 	    "electoral_district": "Browns Bay",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
- 		},
+ 	},
   "4": {
 	    "electoral_district": "Rothesay Bay",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-  		},
+  	},
   "5": {
 	    "electoral_district": "Pinehill",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-  		},
+  	},
   "6": {
 	    "electoral_district": "Albany",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-	  	},
+	},
   "7": {
 	    "electoral_district": "Windsor Park",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-  		},
+  	},
   "8": {
 	    "electoral_district": "Murrays Bay",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
- 	 },
+ 	},
   "9": {
 	    "electoral_district": "Unsworth Heights",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-  		},
+  	},
   "10": {
 	    "electoral_district": "Sunnynook",
 	    "valid_winning_candidate_votes": 100,
 	    "percentage_of_elected_candidate_votes_cast_in_electorate": "100%",
 	    "total_votes_cast": 200,
 	    "party": "National Party"
-  		}	  
+	}
   },
-"max_turnout": "6400"
+   "max_turnout": "6400"
 }
-
